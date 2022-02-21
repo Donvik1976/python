e3de84a179e283f790e3d606c69a71c0c7ff0cdd
Donvik1976 <vdonets76@mail.ru>
2022-02-21 19:44:37 +0600

    Решение второго задания третьего урока

---
+++ Donets_Viktor_dz_3/task_3_2.py
@@ -0,0 +1,30 @@
+# *(вместо задачи 1) Доработать предыдущую функцию в num_translate_adv():
+# реализовать корректную работу с числительными, начинающимися с заглавной
+# буквы — результат тоже должен быть с заглавной. Например:
+# >>> num_translate_adv("One")
+# "Один"
+# >>> num_translate_adv("two")
+# "два"
+
+
+def num_translate_adv(num):
+    num_en = {
+        'one': 'один',
+        'two': 'два',
+        'three': 'три',
+        'four': 'четыре',
+        'five': 'пять',
+        'six': 'шесть',
+        'seven': 'семь',
+        'eight': 'восемь',
+        'nine': 'девять',
+        'ten': 'десять',
+    }
+    if num.istitle():
+        num = num.lower()
+        return num_en.get(num).title()
+    else:
+        return num_en.get(num)
+
+
+print('Перевод:', num_translate_adv(input('Ведите число на английском: ')))
